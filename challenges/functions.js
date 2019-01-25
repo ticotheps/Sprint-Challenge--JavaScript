4d9b34ab316040f63d812b2410b3fa961ef35b0b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-25 12:58:34 -0500

    Completed 'Closures' for functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, callback) {
+  return callback(arg1, arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,27 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  const sum = num1+num2; 
+  return sum;
+}
+console.log(add(1,3));
+
+function multiply(num1, num2) {
+  const product = num1*num2; 
+  return product;
+}
+console.log(multiply(10,3));
+
+function greeting(firstName, lastName) { 
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
+console.log(greeting("Stephon", "Marbury"));
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +47,13 @@
 
 // Explanation: 
 
+//  The function 'nestedFunction()' has the ability to access the variable 'internal', despite 'internal' being outside of its
+//  immediate local scope. First, 'nestedFunction() will search within its OWN local scope for the 'internal' variable.
+//  Upon realizing that the 'internal' variable is not defined within its immediate local scope, it looks 'outward' due to
+//  the concept of 'closure', which can be defined as the combination of, a function and the lexical environment within which it
+//  was declared. So, 'closure' is what allows the 'nestedFunction()' the ability to "reach out" and "grab" the value of the
+//  'internal' variable because 'internal' STILL falls within the same immediate GLOBAL scope (of the function 'myFunction') 
+//  as 'nestedFunction'.
 
 const external = "I'm outside the function";
 
