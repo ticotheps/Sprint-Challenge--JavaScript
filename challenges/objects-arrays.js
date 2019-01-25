69a33bd3b958928ece83992d426faa908ea26836
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-25 11:24:20 -0500

    Created 'roar' method for tyrannosaurus object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,7 +13,10 @@ const tyrannosaurus = {
   diet: "carnivorous",
   weight: "7000kg",
   length: "12m",
-  period: "Late Cretaceious"
+  period: "Late Cretaceious",
+  roar: function() {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -51,10 +54,8 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-
-
-console.log();
+// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
